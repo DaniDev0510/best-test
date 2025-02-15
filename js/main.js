f9a7f4a5bd4cbc8e4c58b14f12fd3b0ca74d247c
DaniDev0510 <nogamer0510@gmail.com>
2025-02-15 01:19:04 +0100

    modified

--- js/main.js
+++ js/main.js
@@ -42,11 +42,11 @@
 
     // Header carousel
     $(".header-carousel").owlCarousel({
-        autoplay: true,
+     
         smartSpeed: 1500,
         items: 1,
         dots: true,
-        loop: true,
+       
         nav : true,
         navText : [
             '<i class="bi bi-chevron-left"></i>',
